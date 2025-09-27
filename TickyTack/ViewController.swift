465028c1aea7d958dc6e340f8ea32c936b9b138b
Chase Angelo Giles <thatoneguyfromutah@gmail.com>
2025-09-27 13:05:07 -0600

    Initial Commit

---
+++ TickyTack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TickyTack
+//
+//  Created by Chase Angelo Giles on 9/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TickyTackTests/TickyTackTests.swift
@@ -0,0 +1,17 @@
+//
+//  TickyTackTests.swift
+//  TickyTackTests
+//
+//  Created by Chase Angelo Giles on 9/27/25.
+//
+
+import Testing
+@testable import TickyTack
+
+struct TickyTackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
